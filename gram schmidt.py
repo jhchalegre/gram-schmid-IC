3bad92d8879807d5f38b447cb205216506d0826f
jhchalegre <jhchalegre@gmail.com>
2025-09-30 21:41:58 -0300

    Update gram schmidt.py

--- gram schmidt.py
+++ gram schmidt.py
@@ -1,3 +1,12 @@
+tamanho_vetores = int(input("digite o tamanho do vetor: "))
+vetor = []
+vetores = []
+
+for i in range(tamanho_vetores):
+    vetor = input().split(" ")
+    
+    vetores.append(vetor)
+
 def produto_escalar(vetor1, vetor2):
     return  (x1*x2 + y1*y2)
 
@@ -6,3 +15,5 @@ def projecao(vetor1, vetor2):
 
 def norma (vetor_unico):
     return sqrt(vetor_unico, vetor_unico)
+
+print(vetores)
